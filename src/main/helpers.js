237f2865ba074eb2980c087281ed058472056168
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2019-02-07 07:30:38 +0200

    Implement xml helpers

--- src/main/helpers.js
+++ src/main/helpers.js
@@ -1,3 +1,74 @@
+import path from 'path'
+import fs from 'fs'
+import fse from 'fs-extra'
+import xmlConverter from 'xml-js'
+
+export function loadFile(filePath) {
+	return new Promise((resolve, reject) => fs.readFile(filePath, (err, data) => {
+		if (err) {
+			reject(err)
+			return
+		}
+
+		resolve(data)
+	}))
+}
+
+export async function saveFile(filePath, data) {
+	const dir = path.dirname(filePath)
+
+	if (!await fse.pathExists(dir)) {
+		await fse.mkdirp(dir)
+	}
+
+	return new Promise((resolve, reject) => fs.writeFile(filePath, data, err => {
+		if (err) {
+			reject(err)
+			return
+		}
+		resolve()
+	}))
+}
+
+export async function loadFileText(filePath, encoding = 'utf8') {
+	const buffer = await loadFile(filePath)
+	if (!buffer) {
+		return null
+	}
+	return buffer.toString(encoding)
+}
+
+export function saveFileText(filePath, text) {
+	const buffer = Buffer.from(text)
+
+	return saveFile(filePath, buffer)
+}
+
+async function loadXml(filePath) {
+	const xml = await loadFileText(filePath)
+	return parseXml(xml)
+}
+
+function saveXml(filePath, jsObject) {
+	const xml = buildXml(jsObject)
+	return saveFileText(filePath, xml)
+}
+
+function parseXml(xmlText) {
+	return xmlConverter.xml2js(xmlText, {compact: true})
+}
+
+function buildXml(jsObject) {
+	return xmlConverter.js2xml(jsObject, {compact: true})
+}
+
 export default {
-	test: 'test'
+	loadFile,
+	saveFile,
+	loadFileText,
+	saveFileText,
+	parseXml,
+	buildXml,
+	loadXml,
+	saveXml
 }
--- src/test/tests/main/helpers.js
+++ src/test/tests/main/helpers.js
@@ -1,7 +1,103 @@
 import helpers from '../../../main/helpers'
+import path from 'path'
+import fs from 'fs'
 
 describe('helpers', function () {
-	it('base', function () {
-		assert.strictEqual(helpers.test, 'test')
+	const textXmlPath = require.resolve('../assets/workspace.xml')
+
+	it('loadFile', async function () {
+		const data = await helpers.loadFile(textXmlPath)
+		assert.strictEqual(data.constructor.name, 'Buffer')
+	})
+
+	it('saveFile', async function () {
+		const filePath = path.resolve('tmp/tests/test.txt')
+		const buffer = Buffer.from('Test', 'utf8')
+		if (fs.existsSync(filePath)) {
+			fs.unlinkSync(filePath)
+		}
+		assert.notOk(fs.existsSync(filePath))
+		await helpers.saveFile(filePath, buffer)
+		assert.ok(fs.existsSync(filePath))
+	})
+
+	it('loadFileText', async function () {
+		const data = await helpers.loadFileText(textXmlPath)
+		assert.strictEqual(typeof data, 'string')
+	})
+
+	it('saveFileText', async function () {
+		const filePath = path.resolve('tmp/tests/test.txt')
+		const text = 'Test'
+		if (fs.existsSync(filePath)) {
+			fs.unlinkSync(filePath)
+		}
+		assert.notOk(fs.existsSync(filePath))
+		await helpers.saveFileText(filePath, text)
+		assert.ok(fs.existsSync(filePath))
+	})
+
+	it('save/load file text', async function () {
+		const filePath = path.resolve('tmp/tests/test.txt')
+		const text = 'Test'
+		if (fs.existsSync(filePath)) {
+			fs.unlinkSync(filePath)
+		}
+		assert.notOk(fs.existsSync(filePath))
+		await helpers.saveFileText(filePath, text)
+		assert.ok(fs.existsSync(filePath))
+		const loadedText = await helpers.loadFileText(filePath)
+		assert.strictEqual(loadedText, text)
+	})
+
+	const testXml = '<?xml version="1.0" encoding="utf-8"?>'
+		+ '<note importance="high" logged="true">'
+		+ '<title>Happy</title>'
+		+ '<todo>Work</todo>'
+		+ '<todo>Play</todo>'
+		+ '</note>'
+
+	const testJs = {
+		_declaration: {
+			_attributes: {
+				version : '1.0',
+				encoding: 'utf-8'
+			}
+		},
+		note: {
+			_attributes: {
+				importance: 'high',
+				logged    : 'true'
+			},
+			title: {
+				_text: 'Happy'
+			},
+			todo: [
+				{
+					_text: 'Work'
+				},
+				{
+					_text: 'Play'
+				}
+			]
+		}
+	}
+
+	it('parse xml', function () {
+		assert.deepStrictEqual(helpers.parseXml(testXml), testJs)
+	})
+
+	it('save/load xml', async function () {
+		const filePath = path.resolve('tmp/tests/test.xml')
+		if (fs.existsSync(filePath)) {
+			fs.unlinkSync(filePath)
+		}
+		assert.notOk(fs.existsSync(filePath))
+		await helpers.saveXml(filePath, testJs)
+		assert.ok(fs.existsSync(filePath))
+		const loadedText = await helpers.loadFileText(filePath)
+		assert.strictEqual(loadedText, testXml)
+		const loadedXml = await helpers.loadXml(filePath)
+		assert.deepStrictEqual(loadedXml, testJs)
 	})
 })
