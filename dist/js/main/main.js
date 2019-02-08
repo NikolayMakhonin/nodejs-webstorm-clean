68a004f192eb6c5d967fce5fc7c7de67d3421c35
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2019-02-08 07:02:51 +0200

    Add dist to git for coveralls.io

--- dist/js/main/main.js
+++ dist/js/main/main.js
@@ -3,15 +3,36 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+exports.parseArgs = parseArgs;
 exports.main = main;
 exports.default = void 0;
 
 var _helpers = _interopRequireDefault(require("./helpers/helpers"));
 
+var _yargs = _interopRequireDefault(require("yargs"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function main(args) {
-  console.log(JSON.stringify(args));
+function parseArgs(argv) {
+  // see http://yargs.js.org/docs/
+  const args = (0, _yargs.default)(argv).usage('$0 [input]', '', o => {
+    o.positional('input', {
+      type: 'string',
+      'default': '.idea',
+      describe: '.idea directory'
+    });
+  }).example('$0', 'Run it from project root for quick clean project settings)').options({
+    output: {
+      alias: 'o',
+      describe: 'Output directory'
+    }
+  }).help().argv;
+  return args;
+}
+
+function main(argv) {
+  const args = parseArgs(argv || process.argv);
+  console.log(args);
 }
 
 var _default = {
